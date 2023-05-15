92f8434684ccf30b9ac26db43a81690c801265cf
Fabricio <fabriciooliveirads@hotmail.com>
2023-05-14 21:36:34 -0300

    Initial commit

---
+++ lwjgl3/src/main/kotlin/br/com/fabricio/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,23 @@
+package br.com.fabricio.lwjgl3
+
+import br.com.fabricio.Main
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+
+fun main() {
+    Lwjgl3Application(Main(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("drop")
+        useVsync(true)
+        setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate)
+        setWindowedMode(640, 480)
+        setWindowIcon(
+            "libgdx128.png",
+            "libgdx64.png",
+            "libgdx32.png",
+            "libgdx16.png"
+        )
+    })
+}
+
+
